5bc831350a9adfb9afda4858bc7b38e70354d815
liuliuliuzy <1872405512@qq.com>
2021-04-06 21:30:13 +0800

    first try for #88

--- solutions/88_merge-sorted-array.go
+++ solutions/88_merge-sorted-array.go
@@ -1,3 +1,32 @@
-func merge(nums1 []int, m int, nums2 []int, n int) {
+package solutions
 
+//感觉 似乎可以原地算法？
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	startIndex := 0
+	j := 0
+	for {
+		if startIndex > m-1 || j > n-1 {
+			break
+		}
+		if nums1[startIndex] <= nums2[j] {
+			startIndex += 1
+		} else {
+			nums1[startIndex] = nums2[j]
+			startIndex += 1
+		}
+		j += 1
+	}
+	if j > n-1 {
+		return
+	} else {
+		for j < n {
+			nums1[startIndex] = nums2[j]
+			startIndex += 1
+			j += 1
+		}
+	}
+}
+
+func Merge(nums1 []int, m int, nums2 []int, n int) {
+	merge(nums1, m, nums2, n)
 }
