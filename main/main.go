61de8863b73e239fccba39e5d40dcc342ea77eaa
liuliuliuzy <1872405512@qq.com>
2021-04-06 21:50:54 +0800

    finish #80

--- main/main.go
+++ main/main.go
@@ -6,5 +6,10 @@ import (
 )
 
 func main() {
-	fmt.Println(solutions.IsPathCrossing("NESWW"))
+	nums1 := []int{1, 2, 3}
+	nums2 := []int{}
+	m := 3
+	n := 0
+	solutions.Merge(nums1, m, nums2, n)
+	fmt.Println(nums1)
 }
--- solutions/88_merge-sorted-array.go
+++ solutions/88_merge-sorted-array.go
@@ -2,29 +2,35 @@ package solutions
 
 //感觉 似乎可以原地算法？
 func merge(nums1 []int, m int, nums2 []int, n int) {
-	startIndex := 0
-	j := 0
+	res := make([]int, m+n)
+	i, j, s := 0, 0, 0
 	for {
-		if startIndex > m-1 || j > n-1 {
+		if i > m-1 || j > n-1 {
 			break
 		}
-		if nums1[startIndex] <= nums2[j] {
-			startIndex += 1
+		if nums1[i] < nums2[j] {
+			res[s] = nums1[i]
+			i += 1
 		} else {
-			nums1[startIndex] = nums2[j]
-			startIndex += 1
-		}
-		j += 1
-	}
-	if j > n-1 {
-		return
-	} else {
-		for j < n {
-			nums1[startIndex] = nums2[j]
-			startIndex += 1
+			res[s] = nums2[j]
 			j += 1
 		}
+		s += 1
 	}
+	if i > m-1 {
+		for j < n {
+			res[s] = nums2[j]
+			j += 1
+			s += 1
+		}
+	} else {
+		for i < m {
+			res[s] = nums1[i]
+			i += 1
+			s += 1
+		}
+	}
+	copy(nums1, res)
 }
 
 func Merge(nums1 []int, m int, nums2 []int, n int) {
