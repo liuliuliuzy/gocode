55999e44138aeb43eb88aaa698a50ad34611ea1b
liuliuliuzy <1872405512@qq.com>
2021-04-06 21:54:03 +0800

    leave #88 for tomorrow

---
+++ solutions/80_remove-duplicates-from-sorted-array-ii.go
@@ -0,0 +1,15 @@
+package solutions
+
+func removeDuplicates(nums []int) int {
+	res := 0
+	i := 0
+	nowNum = nums[0]
+	for _, num := range nums {
+		if 
+	}
+	return res
+}
+
+func RemoveDuplicates(nums []int) int {
+	return removeDuplicates(nums)
+}
