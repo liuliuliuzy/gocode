8d76a8e4e13cc3fdc56a25179ddaaa823f3d392a
liuliuliuzy <1872405512@qq.com>
2021-04-15 19:21:18 +0800

    gitignore! it works

--- solutions/30_substring-with-concatenation-of-all-words.go
+++ solutions/30_substring-with-concatenation-of-all-words.go
@@ -12,7 +12,12 @@ func findSubstring(s string, words []string) []int {
 	for _, item := range words {
 		words_ht[item] += 1
 	}
-	fmt.Print(words_ht)
+	// fmt.Print(words_ht)
+	total_len := len(words) * len(words[0])
+	for i := 0; i < len(s)-total_len; i++ {
+		tmp_s := s[i : i+total_len]
+		fmt.Println(tmp_s)
+	}
 	return res
 }
 
