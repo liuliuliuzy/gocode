1fe45801a6976dd40323b9fce61b69d4e29d76c4
liuliuliuzy <1872405512@qq.com>
2021-04-15 18:14:57 +0800

    easy problem #485

---
+++ solutions/485_max-consecutive-ones.go
@@ -0,0 +1,11 @@
+package solutions
+
+func findMaxConsecutiveOnes(nums []int) int {
+	res := 0
+
+	return res
+}
+
+func FindMaxConsecutiveOnes(nums []int) int {
+	return findMaxConsecutiveOnes(nums)
+}
