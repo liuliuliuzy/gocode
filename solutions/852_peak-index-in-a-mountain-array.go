312b813a6c7d12fc66cedd52ff86b0d0a17423e8
liuliuliuzy <1872405512@qq.com>
2021-06-15 22:15:31 +0800

    二分吧

---
+++ solutions/852_peak-index-in-a-mountain-array.go
@@ -0,0 +1,11 @@
+package solutions
+
+func PeakIndexInMountainArray(arr []int) int {
+	return peakIndexInMountainArray(arr)
+}
+
+/*返回山脉数组的峰顶元素索引*/
+//最先想到：二分？
+func peakIndexInMountainArray(arr []int) int {
+
+}
