54ede83d0264fad7cd7d22acbd2674ce6f58203a
liuliuliuzy <1872405512@qq.com>
2021-06-15 22:31:51 +0800

    简单二分

--- solutions/852_peak-index-in-a-mountain-array.go
+++ solutions/852_peak-index-in-a-mountain-array.go
@@ -7,5 +7,22 @@ func PeakIndexInMountainArray(arr []int) int {
 /*返回山脉数组的峰顶元素索引*/
 //最先想到：二分？
 func peakIndexInMountainArray(arr []int) int {
-
+	start := 0
+	end := len(arr)
+	mid := (start + end) / 2
+	for start < end {
+		// fmt.Println(start, end, mid)
+		if arr[mid-1] > arr[mid] {
+			end = mid
+			mid = (start + end) / 2
+			continue
+		}
+		if arr[mid+1] > arr[mid] {
+			start = mid
+			mid = (start + end) / 2
+			continue
+		}
+		break
+	}
+	return mid
 }
