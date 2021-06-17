3ede3ea957a23db47c4ceac18dd377e6798c2956
liuliuliuzy <1872405512@qq.com>
2021-06-17 15:53:47 +0800

    copy

--- solutions/877_stone-game.go
+++ solutions/877_stone-game.go
@@ -5,6 +5,25 @@ func StoneGame(piles []int) bool {
 }
 
 // 又是这种决策型的题目
+// 下面抄的官方题解
 func stoneGame(piles []int) bool {
-	return false
+	length := len(piles)
+	dp := make([][]int, length)
+	for i := 0; i < length; i++ {
+		dp[i] = make([]int, length)
+		dp[i][i] = piles[i]
+	}
+	for i := length - 2; i >= 0; i-- {
+		for j := i + 1; j < length; j++ {
+			dp[i][j] = max(piles[i]-dp[i+1][j], piles[j]-dp[i][j-1])
+		}
+	}
+	return dp[0][length-1] > 0
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
 }
