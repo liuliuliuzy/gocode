1a5f9ddd95292b99ac3368645dd5a21467107ac8
liuliuliuzy <1872405512@qq.com>
2021-07-01 10:45:14 +0800

    dp

--- solutions/lcp_07_chuan-di-xin-xi.go
+++ solutions/lcp_07_chuan-di-xin-xi.go
@@ -32,28 +32,43 @@ package solutions
 // }
 
 // 深度优先
+// func numWays(n int, relation [][]int, k int) int {
+// 	// 建图
+// 	edges := map[int][]int{}
+// 	for _, rela := range relation {
+// 		edges[rela[0]] = append(edges[rela[0]], rela[1])
+// 	}
+// 	res := 0
+// 	// 注意，如果函数闭包存在自身的递归调用，那么就不能将其 declaration 和 assignment 合并在一起
+// 	var dfs func(int, int)
+// 	dfs = func(start, steps int) {
+// 		if steps == k {
+// 			if start == n-1 {
+// 				res++
+// 			}
+// 			return //终止
+// 		}
+// 		for _, to := range edges[start] {
+// 			dfs(to, steps+1)
+// 		}
+// 	}
+// 	dfs(0, 0)
+// 	return res
+// }
+
+//再记个官方题解的dp解法
 func numWays(n int, relation [][]int, k int) int {
-	// 建图
-	edges := map[int][]int{}
-	for _, rela := range relation {
-		edges[rela[0]] = append(edges[rela[0]], rela[1])
-	}
-	res := 0
-	// 注意，如果函数闭包存在自身的递归调用，那么就不能将其 declaration 和 assignment 合并在一起
-	var dfs func(int, int)
-	dfs = func(start, steps int) {
-		if steps == k {
-			if start == n-1 {
-				res++
-			}
-			return //终止
+	dp := make([]int, n)
+	dp[0] = 1
+	for i := 0; i < k; i++ {
+		next := make([]int, n)
+		for _, r := range relation {
+			src, dst := r[0], r[1]
+			next[dst] += dp[src]
 		}
-		for _, to := range edges[start] {
-			dfs(to, steps+1)
-		}
+		dp = next
 	}
-	dfs(0, 0)
-	return res
+	return dp[n-1]
 }
 
 func NumWays(n int, relation [][]int, k int) int {
