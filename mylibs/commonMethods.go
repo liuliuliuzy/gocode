1d20830ad771aecac3ac95b2e0e0c0b3897069bc
liuziyang <1872405512@qq.com>
2021-07-03 12:47:54 +0800

    快排

--- mylibs/commonMethods.go
+++ mylibs/commonMethods.go
@@ -40,3 +40,31 @@ func Gcd(a, b int) int {
 		}
 	}
 }
+
+func QuickSort(data []int, start, end int) {
+	if start < end {
+		midIndex := findIndex(data, start, end)
+		QuickSort(data, start, midIndex-1)
+		QuickSort(data, midIndex+1, end)
+	}
+}
+
+func findIndex(data []int, start, end int) int {
+	//哨兵元素
+	tmp := data[start]
+	l := start
+	r := end
+	for l < r {
+		for l < r && data[r] >= tmp {
+			r--
+		}
+		data[l] = data[r]
+		for l < r && data[l] <= tmp {
+			l++
+		}
+		data[r] = data[l]
+	}
+	//放置哨兵元素
+	data[l] = tmp
+	return l
+}
