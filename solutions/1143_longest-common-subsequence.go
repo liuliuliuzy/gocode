9a9ab8ec22b95b3b5b6d6006dff2dd1ea62ba49e
liuziyang <1872405512@qq.com>
2021-07-26 13:00:24 +0800

    典型动态规划

---
+++ solutions/1143_longest-common-subsequence.go
@@ -0,0 +1,24 @@
+package solutions
+
+//寻找两个字符串的最长公共子序列，动态规划
+func longestCommonSubsequence(text1 string, text2 string) int {
+	m, n := len(text1), len(text2)
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+	for i, c1 := range text1 {
+		for j, c2 := range text2 {
+			if c1 == c2 {
+				dp[i+1][j+1] = dp[i][j] + 1
+			} else {
+				dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+			}
+		}
+	}
+	return dp[m][n]
+}
+
+func LongestCommonSubsequence(text1, text2 string) int {
+	return longestCommonSubsequence(text1, text2)
+}
