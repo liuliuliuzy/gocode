2f6ae20f27eeb3852773807f95439051ccb97e9c
liuziyang <1872405512@qq.com>
2021-11-05 11:10:53 +0800

    good

---
+++ solutions/1218_longest-arithmetic-subsequence-of-given-difference.go
@@ -0,0 +1,16 @@
+package solutions
+
+//tags: array, hash, dynamic programming
+func longestSubsequence(arr []int, difference int) (ans int) {
+	//I think it's a tricky use of hash table in go
+	dp := map[int]int{} //create a map, same a **dp := make(map[int]int)**
+	for _, v := range arr {
+		dp[v] = dp[v-difference] + 1
+		if dp[v] > ans {
+			ans = dp[v]
+		}
+	}
+}
+func LongestSubsequence(arr []int, difference int) int {
+	return longestSubsequence(arr, difference)
+}
