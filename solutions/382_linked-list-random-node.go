32f209dd756dd357b0c359362043bc96d6bd8470
zyleo@wsl2-ubuntu20.10 <1872405512@qq.com>
2022-01-16 11:02:57 +0800

    水塘抽样算法

--- solutions/382_linked-list-random-node.go
+++ solutions/382_linked-list-random-node.go
@@ -2,23 +2,49 @@ package solutions
 
 import "math/rand"
 
+// import "math/rand"
+//
+// type Solution struct {
+// 	length   int
+// 	elements []int
+// }
+//
+// func Constructor(head *ListNode) Solution {
+// 	s := Solution{length: 0, elements: []int{}}
+// 	for head != nil {
+// 		s.elements = append(s.elements, head.Val)
+// 		s.length++
+// 		head = head.Next
+// 	}
+// 	return s
+// }
+//
+// func (s *Solution) GetRandom() int {
+// 	// generate random step
+// 	sample := rand.Intn(s.length)
+// 	return s.elements[sample]
+// }
+
 type Solution struct {
-	length   int
-	elements []int
+	lkst *ListNode
 }
 
 func Constructor(head *ListNode) Solution {
-	s := Solution{length: 0, elements: []int{}}
-	for head != nil {
-		s.elements = append(s.elements, head.Val)
-		s.length++
-		head = head.Next
-	}
-	return s
+	return Solution{head}
 }
 
 func (s *Solution) GetRandom() int {
-	// generate random step
-	sample := rand.Intn(s.length)
-	return s.elements[sample]
+	// 水塘抽样
+	help := s.lkst
+	index := 2
+	value := help.Val
+	for help.Next != nil {
+		help = help.Next
+		index++
+		r := rand.Intn(index)
+		if r == 0 {
+			value = help.Val
+		}
+	}
+	return value
 }
