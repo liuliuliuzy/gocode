d3cbe0a387547e63504e2706318dcc52d0fbdcf4
zyleo <liuzy199901@gmail.com>
2022-11-06 10:48:12 +0800

    completed n521

--- pkg/521_longest_uncommon_subsequence_i/solution.go
+++ pkg/521_longest_uncommon_subsequence_i/solution.go
@@ -1,11 +1,21 @@
 package longestuncommonsubsequencei
 
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return b
+}
+
 /*
 寻找两个字符串中【最长独特子序列】的长度
 【最长独特子序列】定义：该序列为某字符串独有的最长子序列（即不能是其他字符串的子序列）。
 */
 func findLUSlength(a, b string) int {
-
+	if a != b {
+		return max(len(a), len(b))
+	}
+	return -1
 }
 
 func FindLUSlength(a, b string) int {
